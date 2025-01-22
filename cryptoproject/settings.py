39fc7296d19c3e94db566d8e3bcf7ade29bba869
Rupesh <95015179+Retro6727@users.noreply.github.com>
2025-01-22 18:40:14 +0530

    Update settings.py

--- cryptoproject/settings.py
+++ cryptoproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-24oe-ic0e3ft5t)cf_@*g7lwqzvc9syz=p44vvliqk$2q*53+g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app']
 
 
 # Application definition
